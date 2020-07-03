ef1ca1715f2075a9c2f1922d7a5e96bfd33c337f
hpc <philipchanhk626@gmail.com>
2020-07-03 11:24:46 -0400

    python finished automation/testing

--- root/python/automation.py
+++ root/python/automation.py
@@ -2,4 +2,21 @@ from selenium import webdriver
 
 chrome_browser = webdriver.Chrome('./chromedriver')
 
-print(chrome_browser)
+chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
+
+show_message_button = chrome_browser.find_element_by_class_name('btn-default')
+print(show_message_button.get_attribute('innerHTML'))
+
+assert 'Show Message' in chrome_browser.page_source
+
+text_box = chrome_browser.find_element_by_id('user-message')
+text_box.clear()
+text_box.send_keys('TESTING SELENIUM')
+
+show_message_button.click()
+
+output_message = chrome_browser.find_element_by_id('display')
+
+assert 'TESTING SELENIUM' in output_message.text
+
+chrome_browser.quit()
